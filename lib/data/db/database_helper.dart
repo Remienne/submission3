bf0c7fce907d855039c53a5d731dac53b92e56cf
Muhammad Rizqy Mahardika <rizqymahardika21@gmail.com>
2021-11-22 16:59:19 +0700

    add database_helper.dart

---
+++ lib/data/db/database_helper.dart
@@ -0,0 +1,81 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:submission3/data/model/restaurant.dart';
+
+class DatabaseHelper {
+  static DatabaseHelper? _instance;
+  static Database? _database;
+
+  DatabaseHelper._internal() {
+    _instance = this;
+  }
+
+  factory DatabaseHelper() => _instance ?? DatabaseHelper._internal();
+
+  static const String _tblFavorite = 'favorites';
+
+  Future<Database> _initializeDb() async {
+    var path = await getDatabasesPath();
+    var db = openDatabase(
+      '$path/submission.db',
+      onCreate: (db, version) async {
+        await db.execute('''CREATE TABLE $_tblFavorite (
+             id TEXT PRIMARY KEY,
+             name TEXT,
+             city TEXT,
+             rating TEXT,
+             pictureId TEXT,
+           )     
+        ''');
+      },
+      version: 1,
+    );
+
+    return db;
+  }
+
+  Future<Database?> get database async {
+    if (_database == null) {
+      _database = await _initializeDb();
+    }
+
+    return _database;
+  }
+
+  Future<void> insertBookmark(Restaurant restaurant) async {
+    final db = await database;
+    await db!.insert(_tblFavorite, restaurant.toJson());
+  }
+
+  Future<List<Restaurant>> getBookmarks() async {
+    final db = await database;
+    List<Map<String, dynamic>> results = await db!.query(_tblFavorite);
+
+    return results.map((res) => Restaurant.fromJson(res)).toList();
+  }
+
+  Future<Map> getBookmarkByUrl(String id) async {
+    final db = await database;
+
+    List<Map<String, dynamic>> results = await db!.query(
+      _tblFavorite,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    if (results.isNotEmpty) {
+      return results.first;
+    } else {
+      return {};
+    }
+  }
+
+  Future<void> removeBookmark(String id) async {
+    final db = await database;
+
+    await db!.delete(
+      _tblFavorite,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
